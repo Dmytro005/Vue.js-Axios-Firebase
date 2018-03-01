a0c4e6f9da128e2e228b0f0091c5186d8d4763ac
Dmtro Harazdovskiy <harazdovskiy@gmail.com>
2018-03-02 00:06:19 +0200

    Some simple requests

---
+++ src/components/dashboard/dashboard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="dashboard">
+    <h1>That's the dashboard!</h1>
+    <p>You should only get here if you're authenticated!</p>
+    <pre>
+      {{data}}
+    </pre>
+  </div>
+</template>
+<script>
+  import axios from 'axios';
+
+  export default {
+    data() {
+      return {
+        data: '',
+      };
+    },
+    created() {
+      axios.get('https://vue-axios-9893b.firebaseio.com/users.json')
+      .then(response => { this.data = response.data})
+      .catch(e => console.error(e));
+    }
+  }
+</script>
+<style scoped>
+  h1, p {
+    text-align: center;
+  }
+
+  p {
+    color: red;
+  }
+</style>
