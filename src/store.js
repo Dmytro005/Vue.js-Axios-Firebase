db9363f4eb6204551612ca87e4ef6b84857bcbb4
Dmtro Harazdovskiy <harazdovskiy@gmail.com>
2018-03-03 22:41:50 +0200

    Update UI according to Auth state

--- src/store.js
+++ src/store.js
@@ -89,6 +89,11 @@ export default new Vuex.Store({
   getters: {
     user (state) {
       return state.user
-    }
+    },
+
+    isAuthenticated(state) {
+      return state.idToken !== null;
+    },
+
   }
 })
